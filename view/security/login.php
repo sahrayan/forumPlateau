620ad236a958a24e51327be1393c8c7ea8dd7257
rayan <rayanraihani9@gmail.com>
2023-09-19 11:06:15 +0200

    login

---
+++ view/security/login.php
@@ -0,0 +1,13 @@
+<p>Se connecter</p>
+<form action="index.php?ctrl=security&action=login" method = POST>
+
+    <label >Email</label>
+    <input type ="email" name="email" required placeholder="....@....">
+
+    <label >Mot de Passe</label>
+    <input type="password" name="password" required placeholder="*******">
+
+    <input type="submit" name="submit" >
+    
+
+</form>
