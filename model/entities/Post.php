6b8505a9e088dd19cb2683ae19c2222c191262b1
rayan <rayanraihani9@gmail.com>
2023-09-19 09:26:30 +0200

    post

---
+++ model/entities/Post.php
@@ -0,0 +1,116 @@
+<?php
+    namespace Model\Entities;
+
+    use App\Entity;
+
+    final class Post extends Entity{
+
+        private $id;
+        private $text;
+        private $datePost;
+        private $user;
+        private $topic;
+        
+
+        public function __construct($data){         
+            $this->hydrate($data);        
+        }
+ 
+        /**
+         * Get the value of id
+         */ 
+        public function getId()
+        {
+                return $this->id;
+        }
+
+        /**
+         * Set the value of id
+         *
+         * @return  self
+         */ 
+        public function setId($id)
+        {
+                $this->id = $id;
+
+                return $this;
+        }
+
+        /**
+         * Get the value of text
+         */ 
+        public function getText()
+        {
+                return $this->text;
+        }
+
+        /**
+         * Set the value of text
+         *
+         * @return  self
+         */ 
+        public function setText($text)
+        {
+                $this->text = $text;
+
+                return $this;
+        }
+
+        public function __toString()
+        {
+                return $this->text;
+        }
+
+        /**
+         * Get the value of user
+         */ 
+        public function getUser()
+        {
+                return $this->user;
+        }
+
+        /**
+         * Set the value of user
+         *
+         * @return  self
+         */ 
+        public function setUser($user)
+        {
+                $this->user = $user;
+
+                return $this;
+        }
+
+
+          /**
+         * Get the value of topic
+         */ 
+        public function getTopic()
+        {
+                return $this->topic;
+        }
+
+        /**
+         * Set the value of topic
+         *
+         * @return  self
+         */ 
+        public function setTopic($topic)
+        {
+                $this->topic = $topic;
+
+                return $this;
+        }
+
+        public function getDatePost(){
+            $formaDate = $this->datePost->format("d/m/Y, H:i:s");
+            return $formaDate;
+        }
+
+        public function setDatePost($datePost){
+            $this->datePost = new \DateTime($datePost);
+            return $this;
+        }
+
+       
+    }
