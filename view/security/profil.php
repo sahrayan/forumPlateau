0afd2f85a425708f4d1ada117cc346b16e23cb8f
rayan <rayanraihani9@gmail.com>
2023-09-19 13:31:14 +0200

    profil

--- view/security/profil.php
+++
@@ -1,6 +0,0 @@
-<div id="user-profile">
-    <h2>Mon Profil</h2>
-    <p><strong>Pseudo:</strong> <?= App\Session::getUser() ?></p>
-    <p><strong>Email:</strong> <?= App\Session::getEmail() ?></p>
-    <p><strong>Date de création du compte:</strong> <?= App\Session::getRegistrationDate() ?></p>
-</div>
