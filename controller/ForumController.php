7d51d3571a83ac5634f78a27d45965378ecc666a
rayan <rayanraihani9@gmail.com>
2023-09-19 14:48:55 +0200

    add

--- controller/ForumController.php
+++ controller/ForumController.php
@@ -71,23 +71,27 @@ class ForumController extends AbstractController implements ControllerInterface
         }
     }
 
+    
     // Méthode pour ajouter une catégorie
     public function addCategory()
     {
+        // Vérifiez si l'utilisateur est connecté et a le rôle d'administrateur
+    if (isset($_SESSION['user']) && $_SESSION['user']->getRole() == 'admin') {
         $categoryManager = new CategoryManager();
+        $categoryName = filter_input(INPUT_POST, "categoryName", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
 
-        if (isset($_POST['submit']) && isset($_SESSION['user']) && $_SESSION['user']->getRole() == 'admin') {
-            $categoryName = filter_input(INPUT_POST, "categoryName", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-            
-            if ($categoryName) {
-                $newCategory = $categoryManager->add(["categoryName" => $categoryName]);
-                $this->redirectTo('forum', 'listCategories', $newCategory);
-            }
-        } else {                          
-            $_SESSION["error"] = "Vous n'avez pas l'autorisation d'ajouter une Catégorie";
-            $this->redirectTo('forum', 'listCategories');
+        if ($categoryName) {
+            // Ajoutez la nouvelle catégorie à la base de données
+            $newCategory = $categoryManager->add(["categoryName" => $categoryName]);
+            $this->redirectTo('forum', 'listCategories', $newCategory);
         }
+    } else {
+        $_SESSION["error"] = "Vous n'avez pas l'autorisation d'ajouter une Catégorie";
+        $this->redirectTo('forum', 'listCategories');
     }
+    }
+
+    
 
     // Méthode pour ajouter un sujet
     public function addTopic($id)
@@ -97,33 +101,38 @@ class ForumController extends AbstractController implements ControllerInterface
         $idcategory = $categoryManager->findOneById($id)->getId();
         $userManager = new UserManager();
         
-        if (isset($_SESSION['user']) && isset($_POST['submit']) && !$_SESSION['user']->getRole() == 'ban' || $_SESSION['user']->getRole() == 'admin') {
+        // Vérifiez si l'utilisateur est connecté
+        if (isset($_SESSION['user']) && isset($_POST['submit']) && !$_SESSION['user']->getRole() == 'admin') {
             $idUser = $_SESSION['user']->getId();
             $locked = 0;
             $topicName = filter_input(INPUT_POST, "topicName", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
             
             if ($topicName) {
+                // Ajoutez le nouveau sujet à la base de données
                 $newTopic = $topicManager->add(['topicName' => $topicName, 'category_id' => $idcategory, 'user_id' => $idUser, 'locked' => 0]);
                 $this->redirectTo('forum', 'listCategories', $newTopic);
             }
         } else { 
-            $_SESSION["error"] = "Vous êtes Banni";
+            $_SESSION["error"] = "Vous êtes Banni ou n'avez pas l'autorisation de créer un sujet";
             $this->redirectTo('forum', 'listCategories');
         }
     }
+    
 
-    // Méthode pour ajouter un message
-    public function addPost($id)
-    {
-        $postManager = new PostManager();
-        $topicManager = new TopicManager();
-        $idTopic = $topicManager->findOneById($id)->getId();
-        $statut = $_SESSION['user']->getBan();
+// Méthode pour ajouter un message
+public function addPost($id)
+{
+    $postManager = new PostManager();
+    $topicManager = new TopicManager();
+    $idTopic = $topicManager->findOneById($id)->getId();
+    
+    // Vérifiez si l'utilisateur est connecté
+    if (isset($_SESSION['user'])) {
+        $idUser = $_SESSION['user']->getId();
+        $postContent = filter_input(INPUT_POST, "text", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
         
-        if (isset($_SESSION['user']) && isset($_POST['submit']) && $statut || $_SESSION['user']->getRole() == 'admin') {
-            $idUser = $_SESSION['user']->getId();
-            $postContent = filter_input(INPUT_POST, "text", FILTER_SANITIZE_FULL_SPECIAL_CHARS);
-            
+        // Vérifiez si l'utilisateur est banni
+        if ($_SESSION['user']->getBan() == 0 || $_SESSION['user']->getRole() == 'admin') {
             if ($postContent) {
                 $newPost = $postManager->add(["text" => $postContent, "topic_id" => $idTopic, "user_id" => $idUser]);
                 $this->redirectTo('forum', 'listCategories', $newPost);
@@ -132,7 +141,12 @@ class ForumController extends AbstractController implements ControllerInterface
             $_SESSION["error"] = "Vous êtes Banni";
             $this->redirectTo('forum', 'listCategories');
         }
+    } else { 
+        $_SESSION["error"] = "Vous devez vous connecter pour ajouter un message";
+        $this->redirectTo('forum', 'listCategories');
     }
+}
+
 
     // Méthode pour supprimer un sujet
     public function deleteTopic($id)
@@ -142,6 +156,7 @@ class ForumController extends AbstractController implements ControllerInterface
         $pseudo = $topic->getUser()->getPseudo();
         $user = $_SESSION['user']->getPseudo();
         
+        // Vérifiez si l'utilisateur est l'auteur du sujet ou un administrateur
         if ($pseudo == $user || $_SESSION['user']->getRole() == 'admin') {
             $topicManager->delete($id);
         } else {
@@ -150,6 +165,7 @@ class ForumController extends AbstractController implements ControllerInterface
         
         $this->redirectTo('forum', "listCategories", $topic);
     }
+    
 
     // Méthode pour supprimer un message
     public function deletePost($id)
@@ -159,6 +175,7 @@ class ForumController extends AbstractController implements ControllerInterface
         $pseudo = $post->getUser()->getPseudo();
         $user = $_SESSION['user']->getPseudo();
         
+        // Vérifiez si l'utilisateur est l'auteur du message ou un administrateur
         if ($pseudo == $user || $_SESSION['user']->getRole() == 'admin') {
             $postManager->delete($id);
         } else {
@@ -167,15 +184,28 @@ class ForumController extends AbstractController implements ControllerInterface
         
         $this->redirectTo('forum', "listCategories", $post);
     }
-
     // Méthode pour supprimer une catégorie
     public function deleteCategory($id)
     {
-        $categoryManager = new CategoryManager();
-        
-        if (isset($_POST['submit']) && isset($_SESSION['user']) && $_SESSION['user']->getRole() == 'admin') {
+        // Vérifiez si l'utilisateur est connecté et a le rôle d'administrateur
+        if (isset($_SESSION['user']) && $_SESSION['user']->getRole() == 'admin') {
+            $categoryManager = new CategoryManager();
+            $category = $categoryManager->findOneById($id);
+
+        if ($category) {
+            // Supprimez la catégorie de la base de données
+            $categoryManager->delete($id);
+        } else {
+            $_SESSION["error"] = "La catégorie spécifiée n'existe pas.";
         }
+    } else {
+        $_SESSION["error"] = "Vous n'avez pas l'autorisation de supprimer une catégorie.";
     }
+
+    $this->redirectTo('forum', 'listCategories');
+}
+
+    
 
 }
 
