561bff7ba0be59597781b6ce2442a5af1e754451
rayan <rayanraihani9@gmail.com>
2023-09-20 16:51:17 +0200

    home

--- view/home.php
+++ view/home.php
@@ -1,9 +1,4 @@
 <h1>BIENVENUE SUR LE FORUM</h1>
-
+<link rel="stylesheet" href="public/css/style.css">
 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit ut nemo quia voluptas numquam, itaque ipsa soluta ratione eum temporibus aliquid, facere rerum in laborum debitis labore aliquam ullam cumque.</p>
 
-<p>
-    <a href="/security/login.html">Se connecter</a>
-    <span>&nbsp;-&nbsp;</span>
-    <a href="/security/register.html">S'inscrire</a>
-</p>
